82545142a06734eff7fd2ca06653646807f0d415
chenkang <lv23ck@hotmail.com>
2019-08-06 17:55:18 +0800

    fix bug

--- test.py
+++ test.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     print('reference images:', ref)
     vis = visdom.Visdom()
     for f in ref:
-        vis.image(np.transpose(cv2.imread(f)[..., ::-1], (2, 0, 1)), win=f)    
+        vis.image(np.transpose(cv2.imread(f)[..., ::-1], (2, 0, 1)), win=f, opt=dict(title='REF_%s'%f.rsplit('/')[-1]))    
 
     with torch.no_grad():
         rec = {f.rsplit('/')[-1]: {'top_8': [], 'query': model(transform(Image.open(f).convert('RGB')).unsqueeze(0).cuda(None, non_blocking=True), False).cpu().numpy()} for f in ref}
@@ -52,5 +52,5 @@ if __name__ == '__main__':
                     if update:
                         # update top 8 in visdom
                         imgs = np.concatenate([np.transpose(cv2.resize(cv2.imread(d['fn']), (250, 250))[..., ::-1], (2, 0, 1))[np.newaxis] for d in rec[key]['top_8']])
-                        vis.images(imgs, win=j, nrow=2, opts=dict(title='IMG_%s'%key))
+                        vis.images(imgs, win=key, nrow=2, opts=dict(title='IMG_%s'%key))
                         print('Updated %s'%key)
