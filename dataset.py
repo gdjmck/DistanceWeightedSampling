33874323ac9f63f50f8ab4eb8ef5c27282ac407d
chenkang <lv23ck@hotmail.com>
2019-08-13 11:13:03 +0800

    fix bug

--- dataset.py
+++ dataset.py
@@ -1,7 +1,7 @@
 import torchvision.datasets as datasets
 
 class ImageFolderWithName(datasets.ImageFolder):
-    def __init__(self, *args, **kwargs, return_fn=False):
+    def __init__(self, return_fn=False, *args, **kwargs):
         super(datasets.ImageFolder, self).__init__(*args, **kwargs)
         self.return_fn = return_fn
 
