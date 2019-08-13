d75f1e1904530463c05e73ee639765c040c518f3
chenkang <lv23ck@hotmail.com>
2019-08-13 11:10:42 +0800

    new module dataset

---
+++ dataset.py
@@ -0,0 +1,13 @@
+import torchvision.datasets as datasets
+
+class ImageFolderWithName(datasets.ImageFolder):
+    def __init__(self, *args, **kwargs, return_fn=False):
+        super(datasets.ImageFolder, self).__init__(*args, **kwargs)
+        self.return_fn = return_fn
+
+    def __getitem__(self, i):
+        img, label = super(ImageFolderWithName, self).__getitem__(i)
+        if not self.return_fn:
+            return img, label
+        else:
+            return img, label, self.imgs[i]
