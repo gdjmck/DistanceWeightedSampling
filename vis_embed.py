7279f3efe4cdd0675a75d0ea54816c99e2f1c175
chenkang <lv23ck@hotmail.com>
2019-08-29 11:00:32 +0800

    arg parser in vis_embed

--- vis_embed.py
+++ vis_embed.py
@@ -4,10 +4,19 @@ import pickle
 import numpy as np
 from PIL import Image
 import torchvision.transforms as transforms
+import argparse
+
+def parse_args():
+	parser = argparse.ArgumentParser(description='Visualize embedding in tensorboard.')
+	parser.add_argument('--embeddings', type=str, required=True, help='embedding pickle file.')
+	parser.add_argument('--save-folder', type=str, default='./runs', help='directory to save tensorboard file.')
+	return parser.parse_args()
 
 if __name__ == '__main__':
-	writer = SummaryWriter()
-	with open('embedding_toy25_epoch44.pkl', 'rb') as f:
+	args = parse_args()
+	assert args.embeddings.endswith('.pkl')
+	writer = SummaryWriter(args.save_folder)
+	with open(args.embeddings, 'rb') as f:
 		data = pickle.load(f)
 	labels = [key.split('/')[-2] for key in data.keys()]
 	feats = list(data.values())
